6587ae5179d3cc17df5f369685cfd3ecbcf2dda8
MyUsername <alberto.baglivi@gmail.com>
2022-10-27 16:08:12 +0200

    Add Milestone 2

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ createApp({
 
         return{
 
-
         todoList : [ {text: "fare la spesa", done: true}, {text: "stirare", done: false}, {text: "lavare i piatti", done: true}, 
         {text: "lavare i vestiti", done: false}]
 
         }
 
-    }
+    }, 
+
+    methods : {
+
+        delete_item(indexItem){
+             
+             this.todoList.splice(indexItem,1)
+
+            }
+
+        }
+
 
 }).mount("#app")
